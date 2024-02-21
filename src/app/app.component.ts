e3244874785c9ffa79d028bd04e99dd2624661ca
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-02-21 15:49:57 -0300

    Criando lib "design-system" e consumindo componente "botao" na aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BotaoComponent } from '@alfabit-standalone/design-system';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, BotaoComponent],
   selector: 'alfabit-standalone-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
